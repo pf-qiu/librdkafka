a6a46075527e05cb54037091d0807a37ee7bfe3b
Peifeng Qiu <pqiu@pivotal.io>
2018-11-22 16:40:51 +0800

    fix

--- examples/test_n_parts.cpp
+++ examples/test_n_parts.cpp
@@ -25,6 +25,7 @@ struct TopicConsumer
         if (rk == NULL)
         {
             printf("%s\n", errstr);
+            exit(1);
         }
         rd_kafka_brokers_add(rk, brokers);
         rkt = rd_kafka_topic_new(rk, topic, 0);
@@ -83,37 +84,40 @@ struct TopicConsumer
         rd_kafka_consume_stop(rkt, partition);
     }
 
-    void CreateQueues(int partitions, int cores)
+    struct ConsumeData
     {
-        for (int i = 0; i < cores; i++)
-        {
-            queues.emplace_back(rd_kafka_queue_new(rk));
-        }
-        for (int i = 0; i < partitions; i++)
-        {
-            rd_kafka_consume_start_queue(rkt, i, 0, queues[i % cores]);
-        }
-    }
-    void StopQueues(int partitions)
+        bool eof;
+        size_t bytes;
+        int messages;
+    };
+    void ConsumeCallbackWorker(int partition)
     {
-        for (int i = 0; i < partitions; i++)
+        ConsumeData data;
+        data.eof = false;
+
+        while(!data.eof)
         {
-            rd_kafka_consume_stop(rkt, i);
-        }
-        for (rd_kafka_queue_t* q : queues)
-        {
-            rd_kafka_queue_destroy(q);
-        }
-        queues.clear();
-    }
-    void ConsumeQueueWorker(int q)
-    {
-        rd_kafka_queue_t* rkq = queues[q];
-        rd_kafka_message_t* messages[BATCH_SIZE];
-        bool eof = false;
-        while(!eof)
-        {
-            ssize_t count = rd_kafka_consume_batch_queue(rkq, 1000, messages, BATCH_SIZE);
+            data.bytes = 0;
+            data.messages = 0;
+            int count = rd_kafka_consume_callback(rkt, partition, 1000, 
+            [](rd_kafka_message_t* msg, void* opaque){
+                ConsumeData* cd = (ConsumeData*)opaque;
+                if (msg->err)
+                {
+                    cd->eof = true;
+                    if (msg->err == RD_KAFKA_RESP_ERR__PARTITION_EOF)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        printf("message error: %d, %s\n", msg->err, rd_kafka_err2str(msg->err));
+                        return;
+                    }
+                }
+                cd->messages++;
+                cd->bytes += msg->len;
+            }, &data);
             if (count < 0)
             {
                 printf("%s\n", rd_kafka_err2str(rd_kafka_last_error()));
@@ -121,31 +125,20 @@ struct TopicConsumer
             }
             if (count == 0)
                 continue;
-            size_t bytes = 0;
-            for (ssize_t i = 0; i < count; i++)
+
+            if (count != data.messages)
             {
-                rd_kafka_message_t* msg = messages[i];
-                if (msg->err)
-                {
-                    eof = true;
-                    if (msg->err == RD_KAFKA_RESP_ERR__PARTITION_EOF)
-                    {
-                        count--;
-                        break;
-                    }
-                    else
-                    {
-                        printf("message error: %d, %s\n", msg->err, rd_kafka_err2str(msg->err));
-                        count = 0;
-                        break;
-                    }
-                }
-                bytes += msg->len;
-                rd_kafka_message_destroy(msg);
+                printf("mismatch: %d, %d\n", count, data.messages);
             }
-            message_count += count;
-            message_bytes += bytes;
+            message_count += data.messages;
+            message_bytes += data.bytes;
         }
+    }
+    void ConsumeCallback(int partition)
+    {
+        rd_kafka_consume_start(rkt, partition, 0);
+        ConsumeCallbackWorker(partition);
+        rd_kafka_consume_stop(rkt, partition);
     }
     rd_kafka_t* rk;
     rd_kafka_topic_t* rkt;
@@ -214,35 +207,15 @@ int main(int argc, char** argv) {
         cout << "multi" << endl;
         TopicConsumer c;
         vector<thread> threads;
-        int cores = thread::hardware_concurrency();
-
-        if (partitions > cores)
+        for (int i = 0; i < partitions; i++)
         {
-            c.CreateQueues(partitions, cores);
-            for (int i = 0; i < cores; i++)
-            {
-                threads.emplace_back([&c](int q){
-                    c.ConsumeQueueWorker(q);
-                }, i);
-            }
-            for (auto& t : threads)
-            {
-                t.join();
-            }
-            c.StopQueues(partitions);
-        } 
-        else 
+            threads.emplace_back([&c](int part){
+                c.ConsumeCallback(part);
+            }, i);
+        }
+        for (auto& t : threads)
         {
-            for (int i = 0; i < partitions; i++)
-            {
-                threads.emplace_back([&c](int part){
-                    c.Consume(part);
-                }, i);
-            }
-            for (auto& t : threads)
-            {
-                t.join();
-            }
+            t.join();
         }
     }
 
